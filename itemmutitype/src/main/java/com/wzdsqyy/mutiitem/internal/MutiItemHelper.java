1b6772376d2dacb78ce27d428717e15457875b18
Quinta <781538669@qq.com>
2016-11-21 14:54:01 +0800

    修复一个问题

--- itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/MutiItemHelper.java
+++ itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/MutiItemHelper.java
@@ -4,6 +4,9 @@ import android.support.annotation.LayoutRes;
 
 import com.wzdsqyy.mutiitem.MutiItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/11/18.
  */
@@ -21,5 +24,64 @@ public class MutiItemHelper implements MutiItem {
     @Override
     public MutiItemHelper getMutiItem() {
         return this;
+    }
+
+    boolean isSameType(MutiItem other) {
+        if (other.getMutiItem().getMutiItemViewType() == layoutRes) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 在指定索引之后没有此类ViewType了
+     *
+     * @param start
+     * @param list
+     * @return
+     */
+    public static int getNextSameType(int start, List<MutiItem> list) {
+        MutiItem me = list.get(start), node;
+        for (int i = start + 1; i < list.size(); i++) {
+            node = list.get(i);
+            if (me.getMutiItem().isSameType(node)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static ArrayList<MutiItem> getMutiItems(int start, List<MutiItem> mutiItems) {
+        ArrayList<MutiItem> items = new ArrayList<>();
+        MutiItem item = mutiItems.get(start);
+        for (int i = start + 1; i < mutiItems.size(); i++) {
+            MutiItem node = mutiItems.get(i);
+            if (!item.getMutiItem().isSameType(node)) {
+                items.add(node);
+                continue;
+            }
+            if (item.getMutiItem().isSameType(node)) {
+                return items;
+            }
+        }
+        return items;
+    }
+
+    /**
+     * 在指定索引之前没有此类ViewType了
+     *
+     * @param start
+     * @param items
+     * @return
+     */
+    public static int getPreSameType(int start, List<MutiItem> items) {
+        MutiItem me = items.get(start), node;
+        for (int i = start - 1; i > 0; i--) {
+            node = items.get(i);
+            if (me.getMutiItem().isSameType(node)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
