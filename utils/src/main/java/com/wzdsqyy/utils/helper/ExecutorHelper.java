61940e0f035053c7b5d0b6b1ec82de0e8e3b2ee5
Quinta <781538669@qq.com>
2016-11-22 15:39:05 +0800

    no

--- utils/src/main/java/com/wzdsqyy/utils/helper/ExecutorHelper.java
+++ utils/src/main/java/com/wzdsqyy/utils/helper/ExecutorHelper.java
@@ -11,6 +11,7 @@ import com.bumptech.glide.GlideBuilder;
 import org.greenrobot.eventbus.EventBusBuilder;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
