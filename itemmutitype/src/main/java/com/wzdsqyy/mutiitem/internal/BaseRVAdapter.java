c419d9d5530a4b6ccb7c5772b6c0ac108ebfc343
Quinta <781538669@qq.com>
2016-11-22 09:24:48 +0800

    no

--- itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/BaseRVAdapter.java
+++ itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/BaseRVAdapter.java
@@ -14,8 +14,8 @@ import java.util.List;
  * Created by Administrator on 2016/10/13.
  */
 
-public abstract class BaseRVAdapter<VH extends RecyclerView.ViewHolder, M,L extends List<M>> extends RecyclerView.Adapter<VH> {
-    protected L mData;
+public abstract class BaseRVAdapter<VH extends RecyclerView.ViewHolder, M> extends RecyclerView.Adapter<VH> {
+    protected List<M> mData;
     private MutiItemBinderIntercepter intercepter;
 
     public BaseRVAdapter setViewHolderIntercepter(MutiItemBinderIntercepter intercepter) {
@@ -26,28 +26,28 @@ public abstract class BaseRVAdapter<VH extends RecyclerView.ViewHolder, M,L exte
     @Override
     public final VH onCreateViewHolder(ViewGroup parent, int viewType) {
         VH holder = newViewHolder(parent, viewType);
-        if(intercepter!=null){
-            intercepter.afterIntercepter(holder,viewType);
+        if (intercepter != null) {
+            intercepter.afterIntercepter(holder, viewType);
         }
         return holder;
     }
 
-    public void setViewLayoutManager(@NonNull RecyclerView recyclerView){
-        setViewLayoutManager(recyclerView,true);
+    public void setViewLayoutManager(@NonNull RecyclerView recyclerView) {
+        setViewLayoutManager(recyclerView, true);
     }
 
-    public void setViewLayoutManager(@NonNull RecyclerView recyclerView,boolean isVertical) {
+    public void setViewLayoutManager(@NonNull RecyclerView recyclerView, boolean isVertical) {
         LinearLayoutManager manager;
-        if(isVertical){
-            manager = new LinearLayoutManager(recyclerView.getContext(),LinearLayoutManager.VERTICAL,false);
-        }else {
-            manager = new LinearLayoutManager(recyclerView.getContext(),LinearLayoutManager.HORIZONTAL,false);
+        if (isVertical) {
+            manager = new LinearLayoutManager(recyclerView.getContext(), LinearLayoutManager.VERTICAL, false);
+        } else {
+            manager = new LinearLayoutManager(recyclerView.getContext(), LinearLayoutManager.HORIZONTAL, false);
         }
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(this);
     }
 
-    public abstract VH newViewHolder(ViewGroup parent,@LayoutRes int viewType);
+    public abstract VH newViewHolder(ViewGroup parent, @LayoutRes int viewType);
 
     @Override
     public int getItemCount() {
@@ -58,21 +58,11 @@ public abstract class BaseRVAdapter<VH extends RecyclerView.ViewHolder, M,L exte
         return mData.get(position);
     }
 
-    /**
-     * 获取数据集合
-     *
-     * @return
-     */
-    L getData() {
+    public List<M> getData() {
         return mData;
     }
 
-    /**
-     * 添加到顶部
-     * @param data
-     * @return 数量
-     */
-    public int addNewData(L data) {
+    public int addNewData(List<M> data) {
         if (data != null) {
             mData.addAll(0, data);
             return data.size();
@@ -80,118 +70,60 @@ public abstract class BaseRVAdapter<VH extends RecyclerView.ViewHolder, M,L exte
         return 0;
     }
 
-    /**
-     * 在集合尾部添加更多数据集合（上拉从服务器获取更多的数据集合，例如新浪微博列表上拉加载更晚时间发布的微博数据）
-     *
-     * @param data
-     */
-    public int addMoreData(L data) {
-        return addData(data,mData.size()-1);
+    public int addMoreData(List<M> data) {
+        return addData(data, mData.size() - 1);
     }
 
-    public int addData(L data, int position) {
+    public int addData(List<M> data, int position) {
         if (data != null) {
-            mData.addAll(position+1, data);
+            mData.addAll(position + 1, data);
             return data.size();
         }
         return 0;
     }
 
-    /**
-     * 设置全新的数据集合，如果传入null，则清空数据列表（第一次从服务器加载数据，或者下拉刷新当前界面数据表）
-     *
-     * @param data
-     */
-    public void setData(L data) {
+    public void setData(List<M> data) {
         if (data != null) {
             mData = data;
         } else {
             mData.clear();
         }
-        notifyDataSetChanged();
     }
 
-    /**
-     * 清空数据列表
-     */
     public void clear() {
         mData.clear();
         notifyDataSetChanged();
     }
 
-    /**
-     * 删除指定索引数据条目
-     *
-     * @param position
-     */
     public M removeItem(int position) {
         return mData.remove(position);
     }
 
-    /**
-     * 删除指定数据条目
-     *
-     * @param model
-     */
     public boolean removeItem(M model) {
         return (mData.remove(model));
     }
 
-    /**
-     * 在指定位置添加数据条目
-     *
-     * @param position
-     * @param model
-     */
     public int addItem(int position, M model) {
         mData.add(position, model);
         return 1;
     }
 
-    /**
-     * 在集合头部添加数据条目
-     *
-     * @param model
-     */
     public int addFirstItem(M model) {
         return addItem(0, model);
     }
 
-    /**
-     * 在集合末尾添加数据条目
-     *
-     * @param model
-     */
     public int addLastItem(M model) {
         return addItem(mData.size(), model);
     }
 
-    /**
-     * 替换指定索引的数据条目
-     *
-     * @param location
-     * @param newModel
-     */
     public void setItem(int location, M newModel) {
         mData.set(location, newModel);
     }
 
-    /**
-     * 替换指定数据条目
-     *
-     * @param oldModel
-     * @param newModel
-     */
     public void setItem(M oldModel, M newModel) {
         setItem(mData.indexOf(oldModel), newModel);
     }
 
-    /**
-     * 移动数据条目的位置
-     *
-     * @param fromPosition
-     * @param toPosition
-     */
     public void moveItem(int fromPosition, int toPosition) {
         mData.add(toPosition, mData.remove(fromPosition));
     }
--- itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/MutiItemAdapter.java
+++ itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/MutiItemAdapter.java
@@ -1,15 +1,112 @@
 package com.wzdsqyy.mutiitem.internal;
 
-import com.wzdsqyy.mutiitem.MutiItem;
-import com.wzdsqyy.mutiitem.MutiItemBinderFactory;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.wzdsqyy.mutiitem.MutiItem;
+import com.wzdsqyy.mutiitem.MutiItemBinder;
+import com.wzdsqyy.mutiitem.MutiItemBinderFactory;
+import com.wzdsqyy.mutiitem.SpanSize;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 多种视图适配器
  */
-public class MutiItemAdapter<M extends MutiItem> extends AbsMutiItemAdapter<M,List<M>> {
+public class MutiItemAdapter<M extends MutiItem> extends BaseRVAdapter<RecyclerView.ViewHolder,M> {
+    private MutiItemBinderFactory factory;
+    private ArrayList<Class> clazzs;
+    private ArrayList<Integer> itemTypes;
+
     public MutiItemAdapter(MutiItemBinderFactory factory) {
-        super(factory);
+        this.factory = factory;
+        clazzs = new ArrayList<>();
+        itemTypes = new ArrayList<>();
+    }
+
+    public MutiItemAdapter setMutiItemBinderFactory(MutiItemBinderFactory factory) {
+        this.factory = factory;
+        return this;
+    }
+
+    /**
+     * @param clazz     实体类型
+     * @param layoutRes 对应的布局Id
+     * @return
+     */
+    public MutiItemAdapter register(Class<? extends MutiItem> clazz, @LayoutRes int layoutRes) {
+        int index = clazzs.indexOf(clazz);
+        if (index == -1) {
+            clazzs.add(clazz);
+            itemTypes.add(layoutRes);
+        }
+        return this;
+    }
+
+    public boolean isRegister(@LayoutRes int layoutRes) {
+        return itemTypes.indexOf(layoutRes) != -1;
+    }
+
+    /**
+     * 将会替换你设置的布局管理器
+     *
+     * @param recyclerView
+     * @param spanSize
+     */
+    public void diffSpanSizItem(@NonNull RecyclerView recyclerView, @NonNull SpanSize spanSize) {
+        GridLayoutManager manager = new GridLayoutManager(recyclerView.getContext(), spanSize.getSpanCount());
+        DiffSpanSize span = new DiffSpanSize(this, spanSize);
+        manager.setSpanSizeLookup(span);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(this);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder newViewHolder(ViewGroup parent, @LayoutRes int viewType) {
+        if (factory == null) {
+            throw new RuntimeException("必须提前设置 MutiItemBinderFactory");
+        }
+        MutiItemBinder mutiItemBinder = factory.getMutiItemBinder(viewType, parent);
+        RecyclerView.ViewHolder holder;
+        if (mutiItemBinder instanceof RecyclerView.ViewHolder) {
+            holder = (RecyclerView.ViewHolder) mutiItemBinder;
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+            holder = new DefaultMutiItemHolder(view).setMutiItemBinder(mutiItemBinder);
+        }
+        mutiItemBinder.init(holder, this);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        boolean isBinder = holder instanceof DefaultMutiItemHolder;
+        MutiItemBinder binder = isBinder ? ((DefaultMutiItemHolder) holder).getMutiItemBinder() : (MutiItemBinder) holder;
+        binder.onBindViewHolder(getItem(position), position);
+    }
+
+    public int getItemViewType(MutiItem item) {
+        int type = item.getMutiItem().getMutiItemViewType();
+        if (type > 0) {
+            return type;
+        }
+        int index = clazzs.indexOf(item.getClass());
+        if (index != -1) {
+            type = itemTypes.get(index);
+            item.getMutiItem().setMutiItemViewType(type);
+            return type;
+        }
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return getItemViewType(getItem(position));
     }
 }
