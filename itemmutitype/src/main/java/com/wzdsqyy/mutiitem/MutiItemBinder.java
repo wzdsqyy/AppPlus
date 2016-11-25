af1308266086655766f458bfcdaa3c8bec5c85de
Quinta <781538669@qq.com>
2016-11-25 09:16:44 +0800

    优化

--- itemmutitype/src/main/java/com/wzdsqyy/mutiitem/MutiItemBinder.java
+++ itemmutitype/src/main/java/com/wzdsqyy/mutiitem/MutiItemBinder.java
@@ -3,7 +3,9 @@ package com.wzdsqyy.mutiitem;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 
+import com.wzdsqyy.mutiitem.internal.MutiItemAdapter;
+
 public interface MutiItemBinder<D>{
     void onBindViewHolder(D bean, int possion);
-    void init(@NonNull RecyclerView.ViewHolder holder,@NonNull RecyclerView.Adapter adapter);
+    void init(@NonNull RecyclerView.ViewHolder holder,@NonNull MutiItemAdapter adapter);
 }
