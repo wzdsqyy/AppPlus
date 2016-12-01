f8074be887adaa4234d20f6b778c309dc279b91d
Quinta <781538669@qq.com>
2016-12-01 10:48:05 +0800

    updata

--- itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/SectionHelper.java
+++ itemmutitype/src/main/java/com/wzdsqyy/mutiitem/internal/SectionHelper.java
@@ -33,12 +33,11 @@ public class SectionHelper {
     }
 
     /**
-     *
      * @param possion
      * @param adapter
      * @return -1 没有找到，其他在Adapter中的位置
      */
-    public int findSectionPossion(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+    public int findSectionPossion(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
         for (int i = possion; i > 0; i--) {
             MutiItem node = adapter.getData().get(i);
             if (node.getMutiItem().layoutRes == startRes) {
@@ -48,14 +47,14 @@ public class SectionHelper {
         return -1;
     }
 
-    boolean isSection(@IntRange(from = 0) int possion,@NonNull MutiItemAdapter<MutiItem> adapter){
-        if(adapter.getItemViewType(possion)==startRes){
+    boolean isSection(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+        if (adapter.getItemViewType(possion) == startRes) {
             return true;
         }
         return false;
     }
 
-    public int findSectionFooterPossion(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+    public int findSectionFooterPossion(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
         if (endRes == -1) {
             throw new RuntimeException("你没有设置SectionFooter");
         }
@@ -69,7 +68,7 @@ public class SectionHelper {
         return -1;
     }
 
-    public int nextItemPossion(@IntRange(from = 0)int start, @NonNull MutiItem self, @NonNull List<MutiItem> items) {
+    public int nextItemPossion(@IntRange(from = 0) int start, @NonNull MutiItem self, @NonNull List<MutiItem> items) {
         for (int i = start; i < items.size(); i++) {
             if (self.getMutiItem().isBrotherType(items.get(i))) {
                 return i - 1;
@@ -78,16 +77,16 @@ public class SectionHelper {
         return -1;
     }
 
-    public List<MutiItem> getContents(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
-        int start=possion;
-        if(!isSection(possion,adapter)){
-           start=findSectionPossion(possion,adapter);
+    public List<MutiItem> getContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+        int start = possion;
+        if (!isSection(possion, adapter)) {
+            start = findSectionPossion(possion, adapter);
         }
         int end;
-        if(endRes!=-1){
+        if (endRes != -1) {
             end = findSectionFooterPossion(possion, adapter);
-        }else {
-            end = nextItemPossion(start,adapter.getItem(start),adapter.getData());
+        } else {
+            end = nextItemPossion(start, adapter.getItem(start), adapter.getData());
         }
         if (end == -1) {
             return adapter.getData().subList(start + 1, adapter.getData().size());
@@ -96,29 +95,29 @@ public class SectionHelper {
         }
     }
 
-    public List<Integer> getContentPossions(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+    public List<Integer> getContentPossions(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
         int start = findSectionPossion(possion, adapter);
         if (start == -1) {
             return Collections.EMPTY_LIST;
         }
         int end;
-        if(endRes!=-1){
+        if (endRes != -1) {
             end = findSectionFooterPossion(possion, adapter);
-        }else {
-            end = nextItemPossion(start,adapter.getItem(start),adapter.getData());
+        } else {
+            end = nextItemPossion(start, adapter.getItem(start), adapter.getData());
         }
-        if(end==-1){
-            end=adapter.getItemCount()-1;
+        if (end == -1) {
+            end = adapter.getItemCount() - 1;
         }
-        List<Integer> list=new ArrayList<>();
-        for (int i = start+1; i < end; i++) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = start + 1; i < end; i++) {
             list.add(i);
         }
         return list;
     }
 
     public int showContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
-        int section=findSectionPossion(possion,adapter);
+        int section = findSectionPossion(possion, adapter);
         if (hasContents(section, adapter)) {
             List<MutiItem> items = childs.get(section, Collections.EMPTY_LIST);
             if (items.isEmpty()) {
@@ -131,22 +130,22 @@ public class SectionHelper {
         return 0;
     }
 
-    public boolean isShowContents(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
-        if (adapter.getItemViewType(possion) != startRes) {
-            throw new IllegalArgumentException("必须是startRes的possion");
+    public boolean isShowContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+        if (!isSection(possion, adapter)) {
+            return true;
         }
         if (possion + 1 >= adapter.getItemCount()) {
             return false;
         }
-        if (adapter.getItemViewType(possion + 1) != startRes) {
+        if (isSection(possion + 1, adapter)) {
             return true;
         }
         return false;
     }
 
-    public boolean hasContents(@IntRange(from = 0)int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
-        if (adapter.getItemViewType(possion) != startRes) {
-            throw new IllegalArgumentException("必须是startRes的possion");
+    public boolean hasContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+        if (!isSection(possion, adapter)) {
+            return true;
         }
         List<MutiItem> items = childs.get(possion, null);
         if (items != null) {
@@ -156,39 +155,52 @@ public class SectionHelper {
             return false;
         }
         if (endRes == -1) {
-            return adapter.getItemViewType(possion + 1) == startRes;
+            return isSection(possion + 1, adapter);
         } else {
-            return adapter.getItemViewType(possion + 1) == endRes;
+            return isFooter(possion + 1, adapter);
         }
     }
 
-    public int hideContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
-        if (adapter.getItemViewType(possion) != startRes) {
-            throw new IllegalArgumentException("必须是startRes的possion");
+    private boolean isFooter(@IntRange(from = 0) int possion, MutiItemAdapter<MutiItem> adapter) {
+        if (adapter.getItemViewType(possion) == endRes) {
+            return true;
         }
-        List<MutiItem> items = getContents(possion, adapter);
+        return false;
+    }
+
+    public int hideContents(@IntRange(from = 0) int possion, @NonNull MutiItemAdapter<MutiItem> adapter) {
+        int section = possion;
+        if (!isSection(possion, adapter)) {
+            section = findSectionPossion(possion, adapter);
+        }
+        List<MutiItem> items = getContents(section, adapter);
         adapter.getData().removeAll(items);
         childs.put(possion, items);
         return items.size();
     }
 
-    public static void addItems(MutiItem section, List<MutiItem> childs, @NonNull List<MutiItem> datas) {
+    public static int addItems(MutiItem section, List<MutiItem> childs, @NonNull List<MutiItem> datas) {
+        int count = 0;
         if (section != null) {
+            count++;
             datas.add(section);
         }
         if (childs == null) {
-            return;
+            return count;
         }
         datas.addAll(childs);
+        return count + childs.size();
     }
 
-    public static void addItemsAtIndex(@IntRange(from = 0)int index,MutiItem section, List<MutiItem> childs, @NonNull List<MutiItem> items) {
+    public static int addItemsAtIndex(@IntRange(from = 0) int index, MutiItem section, List<MutiItem> childs, @NonNull List<MutiItem> items) {
         int count = 0;
         if (section != null) {
             items.add(index, section);
             count++;
             index++;
         }
+        count = count + (childs == null ? 0 : childs.size());
         items.addAll(index, childs);
+        return count;
     }
 }
